d56c9577972e82f94fcd12c97086a2a65004c7ed
hoangluyn <hoangluyn207@gmail.com>
2022-05-17 11:08:03 +0700

    abc

---
+++ pages/dich-vu/index.js
@@ -0,0 +1,8 @@
+import { Container, Image, VStack, Text, HStack } from "@chakra-ui/react";
+import MailLayout from "../../Components/Layout/MainLayout";
+
+const Index = () => {
+  return <MailLayout isHomePage={true}></MailLayout>;
+};
+
+export default Index;
